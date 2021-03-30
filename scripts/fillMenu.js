42f31de3f5411ff4e30558a54ab912a2bf23c7b4
Vitor Lourenco <vitor.registros@lourencos.net>
2021-03-30 09:21:37 -0300

    Fix img URLs

--- scripts/fillMenu.js
+++ scripts/fillMenu.js
@@ -1,20 +1,20 @@
 let meals = [
   {
-    imgsrc : "../assets/images/decarne.jpeg",
+    imgsrc : "assets/images/decarne.jpeg",
     imgalt : "Pastelao de Carne",
     meal : "Pastelao de Carne",
     description : "Sem misterios, aquele que voce ja conhece",
     price: "R$ 6,90"
   },
   {
-    imgsrc : "../assets/images/degorgonzola.jpg",
+    imgsrc : "assets/images/degorgonzola.jpg",
     imgalt : "Pastel de Gorgozola com Tomate Seco",
     meal : "Pastelao de Gorgonzola com Tomate Seco",
     description : "Experimenta!",
     price: "R$ 9,90"
   },
   {
-    imgsrc : "../assets/images/depalmito.jpg",
+    imgsrc : "assets/images/depalmito.jpg",
     imgalt : "Pastel de Palmito",
     meal : "Pastelao de Palmito",
     description : "Melhor que o da feira! Juro!",
@@ -24,21 +24,21 @@ let meals = [
 
 let drinks = [
   {
-    imgsrc : "../assets/images/coca2l.webp",
+    imgsrc : "assets/images/coca2l.webp",
     imgalt : "Coca 2l",
     meal : "Coca 2l",
     description : "Tem pra todo mundo!",
     price: "R$ 10,00"
   },
   {
-    imgsrc : "../assets/images/cocalata.webp",
+    imgsrc : "assets/images/cocalata.webp",
     imgalt : "Coca 350ml",
     meal : "Coca 350ml",
     description : "Na medida certa",
     price: "R$ 5,00"
   },
   {
-    imgsrc : "../assets/images/matte.webp",
+    imgsrc : "assets/images/matte.webp",
     imgalt : "Matte 1.5l",
     meal : "Matte 1.5l",
     description : "Pra quem curte um cha gelado",
@@ -48,21 +48,21 @@ let drinks = [
 
 let desserts = [
   {
-    imgsrc : "../assets/images/dechocolate.png",
+    imgsrc : "assets/images/dechocolate.png",
     imgalt : "Pastel de Chocolate",
     meal : "Pastel de Chocolate com Morango",
     description : "Indescritivel",
     price: "R$ 8,00"
   },
   {
-    imgsrc : "../assets/images/debanana.webp",
+    imgsrc : "assets/images/debanana.webp",
     imgalt : "Pastel de Banana",
     meal : "Pastel de Banana",
     description : "Inusitadamente brasileira",
     price: "R$ 6,00"
   },
   {
-    imgsrc : "../assets/images/dechocolatebranco.webp",
+    imgsrc : "assets/images/dechocolatebranco.webp",
     imgalt : "Pastel de Chocolate Branco",
     meal : "Pastel de Chocolate Branco",
     description : "Sim, isso existe",
