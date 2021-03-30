99d3061166607587d40b8e5415abeb480b678ad2
Vitor Lourenco <vitor.registros@lourencos.net>
2021-03-29 23:35:41 -0300

    Make select logic js

--- scripts/select.js
+++ scripts/select.js
@@ -1,7 +1,52 @@
-let selected1;
-let selected2;
-let selected3;
+let selectedMeal;
+let selectedDrink;
+let selectedDessert;
 
-function pickOne(group, item, selected){
+function updateCheckoutStatus(){
+  if (!selectedMeal) return;
+  if (!selectedDrink) return;
+  if (!selectedDessert) return;
+} 
+
+let mealGroup = document.getElementById("meals");
+let mealArticles = mealGroup.querySelectorAll("ARTICLE");
+mealArticles.forEach(item => {
+  item.addEventListener("click", (e) => {
+    let optionSelected = e.target;
+    while (optionSelected.tagName != "ARTICLE"){
+      optionSelected = optionSelected.parentNode; 
+    }
+
+    let checky;
+
+    if (optionSelected === selectedMeal){
+      selectedMeal.classList.remove("selected");
+      checky = selectedMeal.querySelector(".checked");
+      checky.classList.add("d-none");
+      selectedMeal = undefined;
+      return;
+    }
+
+    if (selectedMeal){
+      selectedMeal.classList.remove("selected");
+      checky = selectedMeal.querySelector(".checked");
+      checky.classList.add("d-none");
+    }
+
+    selectedMeal = optionSelected;
+    selectedMeal.classList.add("selected");
+    checky = selectedMeal.querySelector(".checked");
+    checky.classList.remove("d-none");
+    
+    updateCheckoutStatus();
+  })
+});
+
+
+
+
+
+
+function pick(group, item){
 
 }
