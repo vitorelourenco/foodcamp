63e7c5475f77faf4fc6f31d9191b64ae80088de8
Vitor Lourenco <vitor.registros@lourencos.net>
2021-03-31 21:02:22 -0300

    Update the way menu items are loaded

--- scripts/fillMenu.js
+++ scripts/fillMenu.js
@@ -2,21 +2,21 @@ const meals = [
   {
     imgsrc : "assets/images/decarne.jpeg",
     imgalt : "Pastelao de Carne",
-    meal : "Pastelao de Carne",
+    name : "Pastelao de Carne",
     description : "Sem misterios, aquele que voce ja conhece",
     price: "R$ 6,90"
   },
   {
     imgsrc : "assets/images/degorgonzola.jpg",
     imgalt : "Pastel de Gorgozola com Tomate Seco",
-    meal : "Pastelao de Gorgonzola com Tomate Seco",
+    name : "Pastelao de Gorgonzola com Tomate Seco",
     description : "Experimenta!",
     price: "R$ 9,90"
   },
   {
     imgsrc : "assets/images/depalmito.webp",
     imgalt : "Pastel de Palmito",
-    meal : "Pastelao de Palmito",
+    name : "Pastelao de Palmito",
     description : "Melhor que o da feira! Juro!",
     price: "R$ 8,90"
   }
@@ -26,21 +26,21 @@ const drinks = [
   {
     imgsrc : "assets/images/coca2l.webp",
     imgalt : "Coca 2l",
-    meal : "Coca 2l",
+    name : "Coca 2l",
     description : "Tem pra todo mundo!",
     price: "R$ 10,00"
   },
   {
     imgsrc : "assets/images/cocalata.webp",
     imgalt : "Coca 350ml",
-    meal : "Coca 350ml",
+    name : "Coca 350ml",
     description : "Na medida certa",
     price: "R$ 5,00"
   },
   {
     imgsrc : "assets/images/matte.webp",
     imgalt : "Matte 1.5l",
-    meal : "Matte 1.5l",
+    name : "Matte 1.5l",
     description : "Pra quem curte um cha gelado",
     price: "R$ 9,00"
   }
@@ -50,78 +50,46 @@ const desserts = [
   {
     imgsrc : "assets/images/dechocolate.png",
     imgalt : "Pastel de Chocolate",
-    meal : "Pastel de Chocolate com Morango",
+    name : "Pastel de Chocolate com Morango",
     description : "Indescritivel",
     price: "R$ 8,00"
   },
   {
     imgsrc : "assets/images/debanana.webp",
     imgalt : "Pastel de Banana",
-    meal : "Pastel de Banana",
+    name : "Pastel de Banana",
     description : "Inusitadamente brasileira",
     price: "R$ 6,00"
   },
   {
     imgsrc : "assets/images/dechocolatebranco.jpg",
     imgalt : "Pastel de Chocolate Branco",
-    meal : "Pastel de Chocolate Branco",
+    name : "Pastel de Chocolate Branco",
     description : "Sim, isso existe",
     price: "R$ 8,10"
   }
 ];
 
-function makeMenuItem (elem, obj) {
-  const node = document.createElement("ARTICLE");
-  node.classList.add("menu-item");
-
-  const itemWrapper = document.createElement("DIV");
-  itemWrapper.classList.add("item-wrapper");
-    const img = document.createElement("IMG");
-    img.src = elem.imgsrc;
-    img.alt = elem.imgalt;
-    itemWrapper.appendChild(img);
-
-    const h3 = document.createElement("H3");
-    const hNode = document.createTextNode(elem.meal);
-    h3.appendChild(hNode);
-    itemWrapper.appendChild(h3);
-
-    const p = document.createElement("P");
-    const pNode = document.createTextNode(elem.description);
-    p.appendChild(pNode);
-    p.classList.add("unit-description");
-    itemWrapper.appendChild(p);
-  node.appendChild(itemWrapper);
-
-  const divWrapper = document.createElement("DIV");
-  divWrapper.classList.add("price-wrapper");
-    const innerP = document.createElement("P");
-    innerP.classList.add("unit-price");
-    const innerPNode = document.createTextNode(elem.price);
-    innerP.appendChild(innerPNode);
-    divWrapper.appendChild(innerP);
-
-    const checkMark = document.createElement("ion-icon");
-    checkMark.classList.add("checked");
-    checkMark.classList.add("d-none");
-    checkMark.name = "checkmark-circle";
-    divWrapper.appendChild(checkMark);
-  node.appendChild(divWrapper);
-
-  obj.appendChild(node);
+function fillMenu (obj, item){
+  const itemImg = obj.querySelector("IMG");
+  itemImg.src = item.imgsrc;
+  itemImg.alt = item.imgalt;
+  const itemName = obj.querySelector("H3");
+  itemName.textContent = item.name;
+  const itemDescription = obj.querySelector(".unit-description");
+  itemDescription.textContent = item.description;
+  const itemPrice = obj.querySelector(".unit-price");
+  itemPrice.textContent = item.price;
 }
 
 const sectionArrs = [meals, drinks, desserts];
 const sectionIDs = ["meals", "drinks", "desserts"];
 
-
 for(let i=0; i<sectionIDs.length; i++){
   const currentSection = document.getElementById(sectionIDs[i]);
-  const currentMenu = currentSection.querySelector(".menu");
-  sectionArrs[i].forEach(elem => makeMenuItem(elem, currentMenu));
-  const spacer = document.createElement("P");
-  spacer.classList.add("spacer");
-  const blankContent = document.createTextNode("_");
-  spacer.appendChild(blankContent);
-  currentMenu.appendChild(spacer);
+  console.log(currentSection);
+  const menuItems = currentSection.querySelectorAll(".menu-item");
+  for(let j=0; j<menuItems.length; j++){
+    fillMenu(menuItems[j],sectionArrs[i][j]);
+  }
 }
